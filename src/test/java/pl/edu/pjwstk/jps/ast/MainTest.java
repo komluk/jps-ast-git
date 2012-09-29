5d1d0007557b0af18ec2ce7e3a31e95cf0e72f28
Paweł Chudzik <pawel.chudzik@gmail.com>
2012-09-29 18:33:50 +0200

    * initial structure

---
+++ src/test/java/pl/edu/pjwstk/jps/ast/MainTest.java
@@ -0,0 +1,11 @@
+package pl.edu.pjwstk.jps.ast;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest {
+	@Test
+	public void mainTest() throws Exception {
+		Assert.assertTrue(true);
+	}
+}
