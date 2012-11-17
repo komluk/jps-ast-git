abc5c91e2ebbcf7de9aa58eec377c99105b1895a
Paweł Chudzik <pawel.chudzik@gmail.com>
2012-11-17 18:32:47 +0100

    * envs frame

---
+++ src/main/java/pl/edu/pjwstk/jps/interpreter/envs/ENVSFrame.java
@@ -0,0 +1,38 @@
+package pl.edu.pjwstk.jps.interpreter.envs;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import edu.pjwstk.jps.interpreter.envs.IENVSBinder;
+import edu.pjwstk.jps.interpreter.envs.IENVSFrame;
+
+public class ENVSFrame implements IENVSFrame {
+	private final List<IENVSBinder> binders = Lists.newArrayList();
+	
+	public ENVSFrame() {}
+	
+	public ENVSFrame(IENVSBinder ... binders) {
+		this(Arrays.asList(binders));
+	}
+	
+	public ENVSFrame(Iterable<IENVSBinder> binders) {
+		for(IENVSBinder b : binders) {
+			this.binders.add(b);
+		}
+	}
+
+	@Override
+	public Collection<IENVSBinder> getElements() {
+		return binders;
+	}
+
+	public ENVSFrame add(IENVSBinder ... binders) {
+		for(IENVSBinder binder : binders) {
+			this.binders.add(binder);
+		}
+		return this;
+	}
+}
