071d8f993d7d9556242cd17c402800d4546a96cc
Paweł Chudzik <pawel.chudzik@gmail.com>
2012-12-01 13:24:08 +0100

    * dodanie metody dodającej elementy

--- src/main/java/pl/edu/pjwstk/jps/result/BagResult.java
+++ src/main/java/pl/edu/pjwstk/jps/result/BagResult.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import com.google.common.collect.Sets;
 
+import edu.pjwstk.jps.result.IAbstractQueryResult;
 import edu.pjwstk.jps.result.IBagResult;
 import edu.pjwstk.jps.result.ISingleResult;
 
@@ -21,6 +22,16 @@ public class BagResult extends CollectionResult<Collection<ISingleResult>> imple
 		super(elements);
 	}
 
+	public void add(IAbstractQueryResult res) {
+		if(res instanceof ISingleResult) {
+			getElements().add((ISingleResult)res);
+		} else if (res instanceof IBagResult) {
+			IBagResult bag = (IBagResult)res;
+			getElements().addAll(bag.getElements());
+		} else {
+			throw new IllegalStateException("unsuported element type: " + res.getClass());
+		}
+	}
 	@Override
 	protected Collection<ISingleResult> initCollection() {
 		return Sets.newHashSet();
