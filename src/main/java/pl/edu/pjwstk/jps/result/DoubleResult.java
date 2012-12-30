10f25248501cc78b64811efe1d5a8ef40f85016e
Paweł Chudzik <pawel.chudzik@gmail.com>
2012-12-30 22:00:14 +0100

    * bonusowy konstruktor

--- src/main/java/pl/edu/pjwstk/jps/result/DoubleResult.java
+++ src/main/java/pl/edu/pjwstk/jps/result/DoubleResult.java
@@ -4,6 +4,10 @@ import edu.pjwstk.jps.result.IDoubleResult;
 
 public class DoubleResult extends SimpleResult<Double> implements IDoubleResult {
 
+	public DoubleResult(Integer value) {
+		super(new Double(value.intValue()));
+	}
+
 	public DoubleResult(Double value) {
 		super(value);
 	}
